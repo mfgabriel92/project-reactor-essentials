e21ed6c044ba679c87bcb823eea3c5cf7e5ee12f
Gabriel <mfgabriel92@gmail.com>
2020-09-26 14:56:54 -0300

    Project setup

---
+++ src/test/java/br/gabriel/reactor/test/MonoTest.java
@@ -0,0 +1,12 @@
+package br.gabriel.reactor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+    @Test
+    public void shouldTest() {
+        log.info("Working as intended");
+    }
+}
