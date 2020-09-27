1e5c13ff6552846a922c8285af2c999ff95f5514
Gabriel <mfgabriel92@gmail.com>
2020-09-27 14:13:35 -0300

    Hot Publisher ConnectableFlux

---
+++ src/test/java/br/gabriel/reactor/test/ConnectableFluxTest.java
@@ -0,0 +1,31 @@
+package br.gabriel.reactor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+@Slf4j
+public class ConnectableFluxTest {
+    @Test
+    public void shouldTestConnectableFlux() {
+        ConnectableFlux<Integer> connectableFlux = Flux
+            .range(1, 10)
+            .log()
+            .delayElements(Duration.ofMillis(100))
+            .publish();
+        
+        connectableFlux.connect();
+    
+        StepVerifier
+            .create(connectableFlux)
+            .then(connectableFlux::connect)
+            .thenConsumeWhile(i -> i <= 5)
+            .expectNext(6, 7, 8, 9, 10)
+            .expectComplete()
+            .verify();
+    }
+}
